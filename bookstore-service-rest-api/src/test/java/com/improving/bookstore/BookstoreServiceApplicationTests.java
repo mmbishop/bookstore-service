b06237adb896dcd84fda96abf016a92baf746685
Mike Bishop <tend2dv8@gmail.com>
2020-02-12 15:34:57 -0600

    Set up a mock BookstoreService in order to be able to run BookstoreServiceApplicationTests as a smoke test.

--- bookstore-service-rest-api/src/test/java/com/improving/bookstore/BookstoreServiceApplicationTests.java
+++ bookstore-service-rest-api/src/test/java/com/improving/bookstore/BookstoreServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.improving.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = BookstoreTestConfiguration.class)
 class BookstoreServiceApplicationTests {
 
     @Test
